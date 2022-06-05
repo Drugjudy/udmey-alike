1a25e70a1b360e3ace1aea592dce1aa19c3d0fb8
Drugjudy <krishkaushal23@gmail.com>
2022-06-05 23:49:51 +0530

    ok

--- src/backend/login-email.js
+++ src/backend/login-email.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "../frontend/student-portal/css/ogin.scss"
+import "../frontend/student-portal/css/login.css"
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -57,16 +57,57 @@ function EmailLog() {
 
   return (
     <>
+
+
+
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
     
-      <form class="login">
-  <input  onChange={(event) => {
+    <div className="title">
+      <h1 >
+        Welcome
+      </h1>
+    </div>
+      
+    <div className="right">
+      
+        <h3>New Student</h3>
+
+        <label for="username">Email</label>
+        <input onChange={(event) => {
             setRegisterEmail(event.target.value);
-          }} type="text" placeholder="Username" />
-  <input  onChange={(event) => {
+          }} type="text" placeholder="Email or Phone" id="username" />
+
+        <label for="password">Password</label>
+        <input onChange={(event) => {
             setRegisterPassword(event.target.value);
-          }} type="password" placeholder="Password" />
-  <button onClick={register}>Login</button>
-</form>
+          }} type="password" placeholder="Password" id="password" />
+
+        <button onClick={register} >Register</button>
+         </div >
+
+        <div className="left">
+        <h3>Student Login</h3>
+         <label for="username">Email</label>
+        <input  onChange={(event) => {
+            setLoginEmail(event.target.value);
+          }} type="text" placeholder="Email or Phone" id="username" />
+
+        <label for="password">Password</label>
+        <input onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}  type="password" placeholder="Password" id="password" />
+
+        <button onClick={login}>Log In</button>
+        <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+        
+   </div> 
+  
+   </div>
+
 
     </>
   )
